63efd45d7c7aa962783dd3b1e87035eff2b3ad3c
Chuck Preslar <chuckpreslar@gmail.com>
2013-02-05 13:58:39 -0500

    Chainable reddit requests wrapped around the superagent module.

--- index.js
+++ index.js
@@ -27,230 +27,124 @@
    * NPM module dependencies.
    */
 
-  var request = require('superagent');
+  var superagent = require('superagent')
+    , Request = superagent.Request;
 
   /**
    * Rereddit module definition. 
    */
 
   var rereddit = module.exports = {};
+
+  /**
+   * Rereddit helpers.
+   */
+
   var base_url = rereddit.base_url = 'http://www.reddit.com/';
-  var regex = rereddit.regex = /t[0-9]+_[a-z0-9]+/g;
-  var types = rereddit.types = {
+  var store = {};
+  var thing = rereddit.thing = {};
+  thing.regex = /t[0-9]+_[a-z0-9]+/g;
+  thing.types = {
       t1: 'comment'
     , t2: 'account'
     , t3: 'link'
     , t4: 'message'
     , t5: 'subreddit'
   };
-  var user = rereddit.user = undefined;
 
   /**
-   * Parse arguments to ensure optional arguments
-   * are interpreted correctly.
-   *
-   * @api private
-   *
-   * @param {Arguments} args The arguments to parse.
-   * @returns {Object} The parsed arguments.
+   * Modifiy the Request constructor for further convenience
+   * methods.
    */
 
-  var _arguments = function(args) {
-    var subreddit, limit, after, callback;
-    for(var arg in args) {
-      switch(typeof args[arg]) {
-        case 'string': {
-          if(args[arg].match(regex))
-            after = args[arg];
-          else if(parseInt(args[arg]))
-            limit = args[arg];
-          else
-            subreddit = args[arg];
-          break;
-        }
-        case 'number': {
-          limit = args[arg];
-          break;
-        }
-        case 'function': {
-          callback = args[arg]
-          break;
-        }
-        default: 
-          break;
-      }
+  if(!Request.prototype.hasOwnProperty('as'))
+    Request.prototype.as = function(user) {
+      if(typeof user === 'undefined' || !user.cookie || !user.data)
+        return this;
+      this.set('cookie', user.cookie);
+      this.send({ uh: user.data.uh });
+      this.query({ uh: user.data.uh });
+      return this;
     }
-    return {
-        subreddit: subreddit
-      , limit: limit
-      , after: after
-      , callback: callback
+
+  if(!Request.prototype.hasOwnProperty('limit'))
+    Request.prototype.limit = function(num) {
+      if(!this.method === 'GET') return this;
+      if(!(num = parseInt(num))) return this;
+      this.query({ limit: num });
+      return this;
     }
+
+  if(!Request.prototype.hasOwnProperty('after'))
+    Request.prototype.after = function(id) {
+      if(!this.method === 'GET') return this;
+      if(id.match(thing.regex)) return this;
+      this.query({ after: id });
+      return this;
+    }
+
+  /**
+   * Overwrite end so we can only send the body of the
+   * request to the callback and set default.
+   */
+
+  Request.prototype._end = Request.prototype.end;
+  Request.prototype.end = function(callback) {
+    var self = this;
+    this.set('accept', 'application/json');
+    this.set('user-agent', 'rereddit - A NodeJS wrapper for reddit.com.');
+    this.query({ 'api_type': 'json' });
+    this._end(function(err, res) {
+      if(!self.authorizing)
+        return callback(err, res.body);
+      if(!res.body.json.data || !res.header['set-cookie'] || res.body.json.errors.length > 0)
+        return callback(new Error('Something went seriously wrong.'), res);
+      return callback(err, { data: res.body.json.data, cookie: res.header['set-cookie'] });
+    });
   };
 
   /**
-   * Generates initial request object.
+   * Initializes a request to read the given item from reddit.com.
    *
-   * @api private
-   *
-   * @param {String} type The request type.
-   * @param {String} url The request url.
+   * @api public
+   * @param {String} [item] The item to read.
    * @returns {Request} The initialized request.
    */
 
-  var _request = function(type, url) {
-    if(arguments.length < 2 || !request[type])
-      throw Error('Something has went horribly wrong.');
-    var req = request[type](url)
-      .set('Accept', 'application/json')
-      .set('User-Agent', 'ReReddit - NodeJS reddit.com API Wrapper.');
-    if(user)
-      req = req.query({ uh: user.modhash })
-            .set('cookie', user['set-cookie']);
+  rereddit.read = function(item) {
+    if(typeof item === 'undefined') return superagent.get(base_url + '.json');
+    if(item.match(thing.regex)) return superagent.get(base_url + 'by_id/' + item + '.json');
+    return superagent.get(base_url + 'r/' + item + '.json');
+  };
+
+  /**
+   * Initializes a request to log a user into reddit.com.
+   *
+   * @api public
+   * @param {String} username The user's username.
+   * @param {String} password The user's password.
+   * @returns {Request} The initialized request.
+   */
+
+  rereddit.login = function(username, password) {
+    if(!username || !password) return this;
+    var req = superagent.post(base_url + 'api/login/' + username)
+      .query({ 'user': username })
+      .query({ 'passwd': password })
+      .send({ passwd: password, user: username, api_type: 'json' });
+    req.authorizing = true;
     return req;
   };
 
   /**
-   * Logs a user into reddit.com using the provided username and password.
+   * Initializes a request to retrieve a user's details from reddit.com.
    *
    * @api public
-   *
-   * @param {String} username The user's username.
-   * @param {String} password The user's password.
-   * @param {Function} callback The callback to fire once the request is resolved.
+   * @returns {Request} The initialized request.
    */
 
-  var login = rereddit.login = function(username, password, callback) {
-    if(arguments.length < 3)
-      throw new Error('rereddit#login expects a username, password, and callback as arguments - none are optional.');
-    _request('post', base_url + 'api/login/' + username)
-      .query({ user: username })
-      .query({ passwd: password })
-      .query({ api_type: 'json'})
-      .send({ passwd: password, user: username, api_type: 'json' })
-      .end(function(err, res) {
-        if(err)
-          return callback(err, undefined);
-        if(res.body.json.errors.length > 0)
-          return callback(res.body.json.errors, undefined);
-        user = rereddit.user = { 'username': username, 'modhash': res.body.json.data.modhash, 'cookie': res.body.json.data.cookie, 'set-cookie': res.header['set-cookie'] }
-        return callback(undefined, user);
-      });
+  rereddit.me = function() {
+    return superagent.get(base_url + 'api/me.json');
   };
-
-  /**
-   * Registers a user on reddit.com using the required credentials and
-   * logs them in.
-   * 
-   * @api public
-   *
-   * NOTE: Not impletemented.
-   */
-
-  var register = rereddit.register = function() {}
-
-  /**
-   * Fetches public details provided about the user.
-   * 
-   * @api public
-   *
-   * @param {String} username The users's username to lookup.
-   * @param {Function} callback The callback to fire once the request is resolved.
-   */
-
-  var about = rereddit.about = function(username, callback) {
-    if(arguments.length < 2)
-      throw new Error('rereddit#about expects a username, and callback as arguments - none are optional.');
-    _request('get', base_url + 'user/' + username + '/about.json')
-      .end(function(err, res) {
-        if(err)
-          return callback(err, undefined);
-        return callback(undefined, res.body)
-      });
-  };
-
-  /**
-   * Fetches details about the current authorized user.
-   *
-   * @api public
-   *
-   * NOTE: Not impletemented.
-   */
-
-  var me = rereddit.me = function(callback) {
-    if(arguments.length < 1)
-      throw new Error('rereddit#me expects a callback as an argument.');
-    _request('get', base_url + 'api/me.json')
-      .end(function(err, res) {
-        if(err)
-          return callback(err, undefined);
-        return callback(undefined, res.body);
-      });
-  };
-
-  /**
-   * Retrieves posts from reddit.com and potential subreddits.
-   *
-   * @api public
-   *
-   * @param {String} [subreddit] The subreddit to retrieve posts from.
-   * @param {Number} [limit] The number of posts to limit the result to.
-   * @param {String} [after] The post all results should follow after.
-   * @param {Function} callback Callback to fire once the request is resolved.
-   */
-
-  var get = rereddit.get = function(subreddit, limit, after, callback) {
-    var args = _arguments(arguments);
-    if(!args.callback)
-      throw new Error('rereddit#get expects at minimum a callback argument.');
-    var url = base_url + (args.subreddit ? 'r/' + args.subreddit : '') + '.json';
-    var req = _request('get', url)
-    if(args.limit)
-      req = req.query({ limit: args.limit })
-    if(args.after)
-      req = req.query({ after: args.after })
-    req.end(function(err, res) {
-      if(err)
-        return args.callback(err, undefined);
-      return args.callback(undefined, res.body);
-    });
-  };
-
-    /**
-     * Retrieves a single post by its id.
-     *
-     * @api public
-     *
-     * @param {String} id The id of the post to retrieve.
-     * @param {Function} callback Callback to fire once the request is resolved.
-     */
-
-    get.by_id = function(id, callback) {
-      _request('get', base_url + 'by_id/' + id + '.json')
-        .end(function(err, res) {
-        if(err)
-          return callback(err, undefined);
-        return callback(undefined, res.body)
-      });
-    };
-
-    /**
-     * Retrieves comments for a signle post.
-     *
-     * @api public
-     *
-     * @param {String} id The id of the post to retrieve comments for.
-     * @param {Function} callback Callback to fire once the request is resolved.
-     */
-
-    get.comments = function(id, callback) {
-      id = id.match(regex) ? id.substr(3) : id;
-      _request('get', base_url + 'comments/' + id + '.json')
-        .end(function(err, res) {
-          if(err)
-            return callback(err, undefined);
-          return callback(undefined, res.body)
-        });
-    };
 
 }())
